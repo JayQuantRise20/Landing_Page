423c1f1c694509c23e368534ad7b45da2f253ff9
Javad Gholizadeh <javad.gholizadeh@veritasadvisory.co.uk>
2022-03-29 16:25:13 +0100

    add new features smooth scroll and preventDefault in event listener

--- js/app.js
+++ js/app.js
@@ -27,7 +27,7 @@
 const fragment = document.createDocumentFragment();
 
 // find the sections in the body of the HTML
-const sections = document.querySelectorAll("section");
+const sections = document.querySelectorAll('section');
 
 
 
@@ -46,18 +46,17 @@ const sections = document.querySelectorAll("section");
 */
 
 // build the nav
-
 for (let i=1; i<=sections.length; i++) {
 
     const element = document.createElement('li'); // create list element in the loop  
     
 
     const nav_link = document.createElement('a'); // create anchor element for each list 
-    nav_link.classList.add("menu__link"); // add class in css file for anchor element 
-    nav_link.innerText = "Section "+i; // add text content to anchor elements
+    nav_link.classList.add('menu__link'); // add class in css file for anchor element 
+    nav_link.innerText = 'Section '+i; // add text content to anchor elements
     
 
-    nav_link.classList.add("section"+i); // add class to the nav links the same as section IDs. 
+    nav_link.classList.add('section'+i); // add class to the nav links the same as section IDs. 
 
 
     element.appendChild(nav_link); 
@@ -80,45 +79,38 @@ function makeActive(){
         if (box.top<=150 && box.bottom>=150){
             // 1. add "your-active-class" to the class list of section in viewport
             j = i+1
-            sections[i].classList.add("your-active-class");
+            sections[i].classList.add('your-active-class');
             //2. add class 'active' to the navbar link asscoicated to the section id
-            const nav_link = document.querySelector(".section"+j);
-            nav_link.classList.add("active");
+            const nav_link = document.querySelector('.section'+j);
+            nav_link.classList.add('active');
 
         }
-
         else {
 
             j = i+1 // section selection as it start from 1 not zero
 
             // 1. remove the your-active-class  from section which is not active
-            sections[i].classList.remove("your-active-class");
+            sections[i].classList.remove('your-active-class');
 
             //2. remove active class from non active nav links
             const nav_link_2 = document.querySelector(".section"+j);
-            nav_link_2.classList.remove("active")
+            nav_link_2.classList.remove('active');
 
         }
-
-
     }
-
 }
 
 
 // Scroll to anchor ID using scrollTO event
 function scroll_to_id(evt){
-
-    
-    // find the class name of section is clicked on navbar
+    // 1. find the class name of section is clicked on navbar
     const anchor_id = evt.target.classList[1];
-    
 
     //2. find the  section id associated with anchor link. 
     const element = document.getElementById(anchor_id);
-    //3. scroll in to the section 
-    element.scrollIntoView();
 
+    //3. scroll in to the section 
+    element.scrollIntoView({behavior:'smooth'});
 }
 
 
@@ -132,20 +124,23 @@ function scroll_to_id(evt){
 
 
 // Scroll to section on link click
-
-const nav_anchors = document.querySelectorAll("a");  // choose all the links in the nav bar
+const nav_anchors = document.querySelectorAll('a');  // choose all the links in the nav bar
 
 
 // loop throught the links and run event listener function 
 for (let i=0;i<nav_anchors.length;i++){
 
-    nav_anchors[i].addEventListener("click",scroll_to_id);
-
-
+    nav_anchors[i].addEventListener('click', function(e){
+        // prevent default events
+        e.preventDefault();
+        // Scroll to section 
+        scroll_to_id(e);
+    });
 }
 
-// Set sections as active
-document.addEventListener("scroll", function() {
+
+// Set sections as active when scrolling down 
+document.addEventListener('scroll', function() {
     makeActive();
   });
 
